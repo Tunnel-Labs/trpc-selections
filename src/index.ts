35dcb495480ac19ec22d88aff6cc0661cf47430e
Leon Si <leon@leondreamed.com>
2023-09-28 20:27:18 -0400

    fix: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export {
 	createSelectionFunction,
-	createWithSelection,
-	defineSelectionMappings,
 	expandSelections
 } from './utils/selection.js';
+export { createWithSelection } from './utils/with-selection.js';
+export { defineSelectionMappings } from './utils/selection-mappings.js';
 export type * from './types/procedure.js';
 export type * from './types/schema.js';
 export type * from './types/selections.js';
